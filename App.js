cb974b2aa14a4d03bd72c57ba03d1bee0771ad32
Dhruv Patel <profdhruv32@gmail.com>
2022-12-07 11:43:43 +0530

    manage position,scale,totation of object

--- App.js
+++ App.js
@@ -14,7 +14,48 @@ import {
 
 const InitialScene=(props)=>{
   let data=props.sceneNavigator.viroAppProps;
+  const [rotation,setRotation]=useState([-45,50,45])
+  const [position,setPosition]=useState([0,0,-100])
+  const [tvScale,setTvScale]=useState([5,5,5])
+  const [skullScale,setSkullScale]=useState([0.5,0.5,0.5])
 
+   const moveObject=(newPosition)=>{
+      console.log(newPosition);
+      setPosition(newPosition)
+
+
+   }
+  
+   const rotateObject=(rotateState, rotationFactor, source)=>{
+      if(rotateState===3){
+        let currentLocation=[rotation[0]-rotationFactor,rotation[1]-rotationFactor,rotation[2]-rotationFactor];
+        setRotation(currentLocation)
+      }
+   }
+
+   const scaleSkullObject=(pinchState, scaleFactor, source)=>{
+       if(pinchState===3){
+        let currentScale=skullScale[0];
+        let newScale=currentScale*scaleFactor;
+        let newScaleArray=[newScale,newScale,newScale]
+         
+        console.log(newScaleArray);
+        setSkullScale(newScaleArray)
+       }
+      
+   }
+
+   const scaleTvObject=(pinchState, scaleFactor, source)=>{
+    if(pinchState===3){
+     let currentScale=tvScale[0];
+     let newScale=currentScale*scaleFactor;
+     let newScaleArray=[newScale,newScale,newScale]
+      
+     console.log(newScaleArray);
+     setTvScale(newScaleArray)
+    }
+   
+}
   ViroMaterials.createMaterials({
   wood:{
     diffuseTexture:require('./assets/wood.jpeg')
@@ -32,20 +73,27 @@ const InitialScene=(props)=>{
       data.object==='TV'?
         <Viro3DObject
         source={require('/Users/dhruvpatel/Documents/code/ReactNative/demo/assets/tv.obj')}
-        position={[0,-1,-3]}
-        scale={[0.5,0.5,0.5]}
+        position={position}
+        scale={tvScale}
+        rotation={rotation}
         type="OBJ"
         materials={['black']}
+        onRotate={rotateObject}
+        onDrag={moveObject}
+        onPinch={scaleTvObject}
+        
       />
       :
       <Viro3DObject
       source={require('/Users/dhruvpatel/Documents/code/ReactNative/demo/assets/skull.obj')}
-      position={[0,0,-100]}
-      rotation={[-45,50,45]}
-      scale={[0.5,0.5,0.5]}
-      materials={['skull']}
-
+      position={position}
+      rotation={rotation}
+      scale={skullScale}
+      materials={['wood']}
+      onDrag={moveObject}
+      onRotate={rotateObject}
       type="OBJ"
+      onPinch={scaleSkullObject}
     />
     }
     
